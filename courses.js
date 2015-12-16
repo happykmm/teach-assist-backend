b82831c7e3a09e11eadf6d94d7b78bb39ab56825
surong <lxdyran@qq.com>
2015-12-17 00:27:38 +0800

    req.query to get GET params

--- courses.js
+++ courses.js
@@ -2,33 +2,32 @@ var express = require("express");
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    console.log("params:");
-    console.log(req.params);
-    console.log("body:");
-    console.log(req.body);
-    console.log("content-type:"+req.get('content-type'));
     var result = {
         code: 0,
         desc: "success!",
         content: []
     };
-    var cursor = req.db.collection("courses").find( { teacher: "jjm"} );
+    var cursor = req.db.collection("courses").find( { teacher: req.query.teacher} );
     cursor.each(function(err, doc) {
         if (err === null) {
-            result.content.push(doc);
-            console.log(doc);
+            if (doc !== null) {
+                result.content.push(doc);
+            } else {
+                res.json(result);
+            }
         } else {
             result.code = 1;
             result.desc = err.toString();
             return false;
         }
     });
-    res.json(result);
 });
 
 router.post('/', function(req, res) {
     console.log("params:");
     console.log(req.params);
+    console.log("query:");
+    console.log(req.query);
     console.log("body:");
     console.log(req.body);
     console.log("content-type:"+req.get('content-type'));
