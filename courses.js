019e8fdd907249cac78f60a0ae5e4dab4b2ffd7e
surong <lxdyran@qq.com>
2015-12-22 00:19:40 +0800

    fit new data structure

--- courses.js
+++ courses.js
@@ -1,13 +1,37 @@
 var router = require("express")();
+var ObjectId = require('mongodb').ObjectId;
+
+router.post('/', function(req, res) {
+    var result = {
+        code: 0,
+        desc: "success!"
+    };
+    if (req.users.type === "teacher")
+    {
+        var name = req.body.name;
+        req.db.collection("courses")
+            .insertOne( { "name":name } )
+            .then(function(insertResult) {
+                req.db.collection("users").updateOne(
+                    { "_id": req.users._id },
+                    { $addToSet:{"courses":insertResult.insertedId} }
+                );
+                res.json(result);
+            });
+    } else {
+        result.code = 1;
+        result.desc = "You don't have the right to add the course!";
+        res.json(result);
+    }
+});
 
 router.get('/', function(req, res) {
-    console.log(req.users);
     var result = {
         code: 0,
         desc: "success!",
         content: []
     };
-    var cursor = req.db.collection("courses").find( { user_id: req.users._id} );
+    var cursor = req.db.collection("courses").find( { _id: { $in : req.users.courses } } );
     cursor.each(function(err, doc) {
         if (err === null) {
             if (doc !== null) {
@@ -24,40 +48,54 @@ router.get('/', function(req, res) {
     });
 });
 
-router.post('/', function(req, res) {
-    var name = req.body.name;
-    var teacher = req.body.teacher;
+router.delete('/', function(req, res) {
+    var course_id = ObjectId(req.query._id);
     var result = {
         code: 0,
         desc: "success!"
     };
-    req.db.collection("courses").insertOne( { "name":name,"teacher":teacher} );
-    res.json(result);
+    if (req.users.type === "teacher")
+    {
+        req.db.collection("users")
+            .updateOne(
+                { "_id": req.users._id },
+                { $pull:{"courses":course_id} }
+            )
+            .then(function (deleteResult) {
+                if (deleteResult.deletedCount>0)
+                {
+                    req.db.collection("courses").deleteOne( { "_id":course_id } );
+                    res.json(result);
+                }
+            })
+    } else {
+        result.code = 1;
+        result.desc = "You don't have the right to delete the course!";
+        res.json(result);
+    }
 });
 
 router.put('/', function(req, res) {
-    var newname=req.body.newname;
-    var teacher=req.body.teacher;
+    var course_id = ObjectId(req.body._id);
+    var newName = req.body.name;
     var result = {
         code: 0,
         desc: "success!"
     };
-    req.db.collection("courses").updateOne
-    (
-        { "teacher": teacher },
-        { $set:{ "name":newname } }
-    );
-    res.json(result);
-});
-
-router.delete('/', function(req, res) {
-    var name=req.query.names;
-    var result = {
-        code: 0,
-        desc: "success!"
-    };
-    req.db.collection("courses").deleteOne( { "name":name } );
-    res.json(result);
+    if (req.users.type === "teacher")
+    {
+        console.log(req.users.type);
+        req.db.collection("courses").updateOne
+        (
+            { "_id": course_id },
+            { $set:{ "name":newName } }
+        );
+        res.json(result);
+    } else {
+        result.code = 1;
+        result.desc = "You don't have the right to update the course!";
+        res.json(result);
+    }
 });
 
 module.exports = router;
