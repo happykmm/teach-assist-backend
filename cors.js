7fe6aae846a7f4d0906a80f10f666fae518fb50d
surong <lxdyran@qq.com>
2015-12-28 10:18:57 +0800

    allow json

--- cors.js
+++ cors.js
@@ -4,7 +4,7 @@ var router = require("express")();
 router.all('*', function(req, res, next) {
     //Allow CORS
     res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Headers', 'x-access-token');
+    res.set('Access-Control-Allow-Headers', 'x-access-token,Content-Type');
     next();
 });
 
