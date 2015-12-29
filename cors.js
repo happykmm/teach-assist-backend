6b959ec40551b2e48f5c1664490cb0decaf4e5ea
surong <lxdyran@qq.com>
2015-12-29 15:20:17 +0800

    allow delete

--- cors.js
+++ cors.js
@@ -5,6 +5,7 @@ router.all('*', function(req, res, next) {
     //Allow CORS
     res.set('Access-Control-Allow-Origin', '*');
     res.set('Access-Control-Allow-Headers', 'x-access-token,Content-Type');
+    res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
 });
 
