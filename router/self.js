31d30e8ec1b1ce9b5b6a99e455b6139cfd21eac9
surong <lxdyran@qq.com>
2016-03-26 20:56:28 +0800

    user mongoose

--- router/self.js
+++ router/self.js
@@ -5,17 +5,16 @@ router.get('/', function(req, res) {
     res.json({code:0, intro: req.users.intro});
 });
 
-router.put('/', function(req, res) {
-    req.db.collection("users")
-        .updateOne(
-            {_id: req.users._id},
-            {$set: {intro: req.body.intro}}
-        ).then(function() {
-            res.json({code:0});
-        }, function(err) {
-            res.json({code:1, desc:err.toString()});
-        })
-
+router.put('/', function(req, res, next) {
+    userModel.update({
+        _id: req.users._id
+    }, {
+        $set: {intro: req.body.intro}
+    }, function(err, result) {
+        if (err) return next(err);
+        // result === {ok: 1, nModified: 1, n:1}
+        res.json({code: 0});
+    });
 });
 
 module.exports = router;
