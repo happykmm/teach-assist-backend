6cc67bdfa493b6e3f2f4896c9731de2fd924f059
surong <lxdyran@qq.com>
2016-04-21 01:44:00 +0800

    self defined time format

--- helper/access-logger.js
+++ helper/access-logger.js
@@ -1,6 +1,7 @@
 var morgan = require('morgan');
 var FileStreamRotator = require('file-stream-rotator');
 var fs = require('fs');
+var moment = require('moment');
 
 var logDir = appRoot + '/log';
 fs.existsSync(logDir) || fs.mkdirSync(logDir);
@@ -20,6 +21,10 @@ morgan.token('x-forwarded-for', function(req) {
     return req.get('X-Forwarded-For');
 });
 
-var logger = morgan('[:date[iso]] :x-forwarded-for ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"', {stream: stream});
+morgan.token('date-time', function() {
+    return moment().format('YYYY/MM/DD HH:mm:ss.SSS');
+});
+
+var logger = morgan('[:date-time] :x-forwarded-for ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"', {stream: stream});
 
 module.exports = logger;
